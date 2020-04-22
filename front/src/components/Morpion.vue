d3c26d4f55b88840c08ac16ff21b79d144d426b8
Vincent Leblond <vincent.leblond@edgemind.net>
2020-04-22 21:36:24 +0200

    fix: Morpion in clean component

--- front/src/components/Morpion.vue
+++ front/src/components/Morpion.vue
@@ -7,23 +7,14 @@
             <v-btn color="primary" @click="launch_die(2)">Lancer</v-btn>
           </v-col>
           <v-col col="1" md="1">
-            <img
-              :src="'static/Dice-' + game.params.dice[0] + '.svg'"
-              height="42"
-              width="42"
-            />
+            <img :src="'static/Dice-' + game.params.dice[0] + '.svg'" height="42" width="42" />
           </v-col>
           <v-col col="1" md="1">
-            <img
-              :src="'static/Dice-' + game.params.dice[1] + '.svg'"
-              height="42"
-              width="42"
-            />
+            <img :src="'static/Dice-' + game.params.dice[1] + '.svg'" height="42" width="42" />
           </v-col>
         </v-row>
         <v-row>
           <v-col col="12" md="12">
-            {{ message }}
             <div class="ma-n2" id="morpionboard">
               <svg
                 :width="game.params.svg_size"
@@ -33,6 +24,12 @@
                 <!-- add board -->
                 <g v-for="item in game.board" :key="item.id">
                   <g :transform="'translate(' + item.x + ', ' + item.y + ')'">
+                    <text
+                      v-if="type=='calcul'"
+                      :x="item.width*0.4"
+                      :y="item.width*0.6"
+                      font-size="40px"
+                    >{{ item.num }}</text>
                     <rect
                       v-show="item.visibility"
                       :id="item.id"
@@ -41,7 +38,7 @@
                       :width="item.width"
                       :height="item.height"
                       style="fill-opacity:0;stroke-width:3;stroke:rgb(0,0,0)"
-                      @click="updateMorpion($event, '')"
+                      @click="updateMorpion($event)"
                     />
                   </g>
                 </g>
@@ -114,16 +111,28 @@
         </v-row>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col col="12" md="12">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" @click="clearMorpion" v-on="on">Rejouer</v-btn>
+          </template>
+          <span>Rejouer</span>
+        </v-tooltip>
+        {{ game.params.end_game_msg }}
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Morpion",
-  props: ["message", "one_screen", "player_order", "mode_morpion", "type"],
+  props: ["one_screen", "mode_morpion", "type"],
   components: {},
   data() {
     return {
+      player_order: 0,
       game: {
         params: {
           dice: [1, 1],
@@ -166,15 +175,21 @@ export default {
       }
     };
   },
-  created() {},
+  created() {
+    if (this.mode_morpion !== null) {
+      this.game.params.mode_morpion = this.mode_morpion;
+    } else {
+      this.game.params.mode_morpion = "normal";
+    }
+  },
   computed: {},
   mounted() {
     this.drawMorpion();
 
     // game update listener
     window.APIClient.on("update", (player, info) => {
-      if (Object.keys(info).indexOf("game") > -1) {
-        this.game = info.game;
+      if (Object.keys(info).indexOf("params") > -1) {
+        this.game = info;
       }
       if (Object.keys(info).indexOf("player_order") > -1) {
         this.player_order = info.player_order;
@@ -237,6 +252,217 @@ export default {
             color: cross.color,
             size: cross.size
           });
+        }
+      }
+    },
+    // clear objects of Morpion
+    clearMorpion() {
+      let n_cell = this.game.params.n_cell;
+      for (let row = 1; row <= n_cell; row++) {
+        for (let col = 1; col <= n_cell; col++) {
+          let objId = row + "_" + col;
+          this.updateObjectVisibility(objId, "circle", false);
+          this.updateObjectVisibility(objId, "cross", false);
+        }
+      }
+      this.game.params.circle.n_checked = 0;
+      this.game.params.cross.n_checked = 0;
+
+      this.game.params.end_game_msg = "";
+      window.APIClient.sendGameUpdate(this.game);
+      this.player_order = 0;
+      window.APIClient.sendGameUpdate({ player_order: 0 });
+    },
+    // update object visibility
+    updateObjectVisibility(objId, objectType, visibility) {
+      for (let c = 0; c < this.game[objectType].length; c++) {
+        if (this.game[objectType][c].id === objectType + "_" + objId) {
+          this.game[objectType][c].visibility = visibility;
+        }
+      }
+      window.APIClient.sendGameUpdate(this.game);
+    },
+    // add an object on board
+    addObject(board_id) {
+      let selectedObjectType = "";
+
+      // get object type to add
+      if (this.one_screen) {
+        if (
+          this.game.params.circle.n_checked === this.game.params.cross.n_checked
+        ) {
+          selectedObjectType = "circle";
+        } else {
+          selectedObjectType = "cross";
+        }
+      } else {
+        // if game start
+        if (
+          this.player_order === 0 &&
+          this.game.params.circle.n_checked === 0
+        ) {
+          selectedObjectType = "circle";
+          this.player_order = 1;
+        } else if (
+          this.player_order === 0 &&
+          this.game.params.circle.n_checked === 1
+        ) {
+          selectedObjectType = "cross";
+          this.player_order = 2;
+        } else if (this.player_order === 1) {
+          selectedObjectType = "circle";
+          if (
+            this.game.params.circle.n_checked > this.game.params.cross.n_checked
+          ) {
+            selectedObjectType = "";
+          }
+        } else if (this.player_order === 2) {
+          selectedObjectType = "cross";
+          if (
+            this.game.params.circle.n_checked ===
+            this.game.params.cross.n_checked
+          ) {
+            selectedObjectType = "";
+          }
+        }
+      }
+
+      // no new object if all filled
+      let n_cell = this.game.params.n_cell;
+      if (
+        this.game.params.circle.n_checked + this.game.params.cross.n_checked ===
+        n_cell * n_cell
+      ) {
+        selectedObjectType = "";
+      }
+
+      // if there is already an object
+      let circle_visibility = this.game.circle.filter(
+        d => d.id === "circle_" + board_id[1] + "_" + board_id[2]
+      )[0].visibility;
+      let cross_visibility = this.game.cross.filter(
+        d => d.id === "cross_" + board_id[1] + "_" + board_id[2]
+      )[0].visibility;
+      if (circle_visibility || cross_visibility) {
+        selectedObjectType = "";
+      }
+
+      if (selectedObjectType !== "") {
+        this.updateObjectVisibility(
+          board_id[1] + "_" + board_id[2],
+          selectedObjectType,
+          true
+        );
+        this.game.params[selectedObjectType].n_checked++;
+      }
+      return selectedObjectType;
+    },
+    // update Morpion
+    updateMorpion(event) {
+      let n_cell = this.game.params.n_cell;
+      let board_id = event.currentTarget.id.split("_");
+      let row_cell = parseInt(board_id[1]);
+      let col_cell = parseInt(board_id[2]);
+
+      let row = row_cell - 1;
+      let col = col_cell - 1;
+      let cell_num = row + 1 + col * n_cell;
+
+      // check allowed to add object
+      // detect winner or draw
+      if (this.game.params.end_game_msg === "") {
+        let selectedObjectType = "";
+        // check if allowed to add an object
+        if (this.type === "calcul") {
+          let dice1 = this.game.params.dice[0];
+          let dice2 = this.game.params.dice[1];
+          if (
+            cell_num === dice1 - dice2 ||
+            cell_num === dice2 - dice1 ||
+            cell_num === dice1 + dice2
+          ) {
+            selectedObjectType = this.addObject(board_id);
+          }
+        } else {
+          selectedObjectType = this.addObject(board_id);
+        }
+
+        if (selectedObjectType !== "") {
+          // check if win or draw
+          let objects = this.game[selectedObjectType];
+
+          let public_name = this.game.params[selectedObjectType].public_name[
+            this.game.params.mode_morpion
+          ];
+
+          // vertical
+          let obj1 = objects.filter(
+            d => d.id === selectedObjectType + "_" + row_cell + "_1"
+          )[0].visibility;
+          let obj2 = objects.filter(
+            d => d.id === selectedObjectType + "_" + row_cell + "_2"
+          )[0].visibility;
+          let obj3 = objects.filter(
+            d => d.id === selectedObjectType + "_" + row_cell + "_3"
+          )[0].visibility;
+          if (obj1 && obj2 && obj3) {
+            this.game.params.end_game_msg = "Victoire de " + public_name;
+          }
+
+          // horizontal
+          obj1 = objects.filter(
+            d => d.id === selectedObjectType + "_1_" + col_cell
+          )[0].visibility;
+          obj2 = objects.filter(
+            d => d.id === selectedObjectType + "_2_" + col_cell
+          )[0].visibility;
+          obj3 = objects.filter(
+            d => d.id === selectedObjectType + "_3_" + col_cell
+          )[0].visibility;
+          if (obj1 && obj2 && obj3) {
+            this.game.params.end_game_msg = "Victoire de " + public_name;
+          }
+
+          // diagonal 1 descendant
+          obj1 = objects.filter(d => d.id === selectedObjectType + "_1_1")[0]
+            .visibility;
+          obj2 = objects.filter(d => d.id === selectedObjectType + "_2_2")[0]
+            .visibility;
+          obj3 = objects.filter(d => d.id === selectedObjectType + "_3_3")[0]
+            .visibility;
+          if (obj1 && obj2 && obj3) {
+            this.game.params.end_game_msg = "Victoire de " + public_name;
+          }
+
+          // diagonal 2 ascendant
+          obj1 = objects.filter(d => d.id === selectedObjectType + "_1_3")[0]
+            .visibility;
+          obj2 = objects.filter(d => d.id === selectedObjectType + "_2_2")[0]
+            .visibility;
+          obj3 = objects.filter(d => d.id === selectedObjectType + "_3_1")[0]
+            .visibility;
+          if (obj1 && obj2 && obj3) {
+            this.game.params.end_game_msg = "Victoire de " + public_name;
+          }
+
+          // all filled
+          if (this.game.params.end_game_msg === "") {
+            if (
+              this.game.params.circle.n_checked +
+                this.game.params.cross.n_checked ===
+              n_cell * n_cell
+            ) {
+              this.game.params.end_game_msg = "Fin de partie : égalité";
+            }
+          }
+
+          // send game update
+          window.APIClient.sendGameUpdate(this.game);
+
+          if (this.game.params.end_game_msg !== "") {
+            this.player_order = 0;
+            window.APIClient.sendGameUpdate({ player_order: 0 });
+          }
         }
       }
     }
